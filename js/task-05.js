1fd7119f6fb390e43e6b6c2308ffbd05efc4880b
Dmytro <dima50066@gmail.com>
2024-05-09 18:03:43 +0200

    task5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeBgColor = document.querySelector(".change-color");
+changeBgColor.addEventListener("click", changeColor);
+const widget = document.querySelector(".task5");
+function changeColor() {
+  const color = getRandomHexColor();
+  widget.style.backgroundColor = color;
+  document.querySelector(".color").textContent = color;
+}
